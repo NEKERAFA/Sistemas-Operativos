2e3b6464a2d6f6e5a8529a103fb5c2a203a54c0f
OoNEKERAFAoO <nekerafa@gmail.com>
2015-10-28 13:18:03 +0100

    new file:   Practica 1/funciones.h 	new file:   Practica 1/lista.c

---
+++ Practica 1/lista.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "funciones.h"
+
+int main(int argc, char * argv[]) {
+    listdir( argc-1, argv+1 );
+    return 0;
+}
